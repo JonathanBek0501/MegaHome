3a9dfa4558b58ceb34600759c3bb27bfe8d73217
Shokhjakhon <shoxjahonsoliyev@gmail.com>
2024-12-09 21:55:29 +0500

    ready

--- components/Page/Home/Populars.vue
+++ components/Page/Home/Populars.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="max-w-7xl mx-auto py-10 px-4 sm:px-6">
         <h2 class="text-4xl font-bold pb-5">
-            Categories
+            Populars
         </h2>
         
         <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-rows-[110px_110px] gap-2">
-            <div class="relative group overflow-hidden lg:row-span-2 flex items-center gap-5 lg:block rounded bg-yellow-500 hover:shadow-brand transition-all ease-in-out p-5">
+            <a href="/products" class="relative group overflow-hidden lg:row-span-2 flex items-center gap-5 lg:block rounded bg-yellow-500 hover:shadow-brand transition-all ease-in-out p-5">
                 <img src="/dishes.svg" alt="dishes" class="hidden sm:block w-16 lg:w-32">
 
                 <div>
@@ -16,19 +16,7 @@
                         345 400na
                     </span>
                 </div>
-
-                <div class="translate-y-full group-hover:translate-y-0 transition-all ease-in-out absolute inset-0 w-full h-full flex flex-col gap-1 items-center justify-center bg-black/50">
-                    <a href="#" class="text-white">
-                        items 1
-                    </a>
-                    <a href="#" class="text-white">
-                        items 1
-                    </a>
-                    <a href="#" class="text-white">
-                        items 1
-                    </a>
-                </div>
-            </div>
+            </a>
             <a href="/products" class="flex items-center gap-5 rounded bg-red-400 hover:shadow-brand transition-all ease-in-out p-5">
                 <img src="/dishes.svg" alt="dishes" class="hidden sm:block w-16">
 
@@ -53,7 +41,7 @@
                     </span>
                 </div>
             </a>
-            <div class="relative group overflow-hidden lg:row-span-2 flex items-center gap-5 lg:block rounded bg-yellow-500 hover:shadow-brand transition-all ease-in-out p-5">
+            <a href="/products" class="relative group overflow-hidden lg:row-span-2 flex items-center gap-5 lg:block rounded bg-yellow-500 hover:shadow-brand transition-all ease-in-out p-5">
                 <img src="/dishes.svg" alt="dishes" class="hidden sm:block w-16 lg:w-32">
 
                 <div>
@@ -64,19 +52,7 @@
                         345 400na
                     </span>
                 </div>
-
-                <div class="translate-y-full group-hover:translate-y-0 transition-all ease-in-out duration-200 absolute inset-0 w-full h-full flex flex-col gap-1 items-center justify-center bg-black/50">
-                    <a href="#" class="text-white">
-                        items 1
-                    </a>
-                    <a href="#" class="text-white">
-                        items 1
-                    </a>
-                    <a href="#" class="text-white">
-                        items 1
-                    </a>
-                </div>
-            </div>
+            </a>
             <a href="/products" class="flex items-center gap-5 rounded bg-red-400 hover:shadow-brand transition-all ease-in-out p-5">
                 <img src="/dishes.svg" alt="dishes" class="hidden sm:block w-16">
 
